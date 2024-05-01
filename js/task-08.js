62b49d6f9a879675fd8c53eb92ba11424ef25ad9
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-05-01 16:06:43 +0300

    tema 8 si 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+function submit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields have to be completed");
+  }
+
+  const result = { email: email.value, password: password.value };
+
+  event.currentTarget.reset();
+
+  console.log(result);
+}
+
+form.addEventListener("submit", submit);
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function result() {
+  backgorund.style.backgroundColor = `${getRandomHexColor()}`;
+  text.textContent = getRandomHexColor();
+}
+
+const button = document.querySelector(".change-color");
+const backgorund = document.querySelector("body");
+const text = document.querySelector(".color");
+
+button.addEventListener("click", result);
